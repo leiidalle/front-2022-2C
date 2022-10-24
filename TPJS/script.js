d6ccc732fe08b096c2943845706e68e8b789c4c4
Leii Dalle <leii16gd@gmail.com>
2022-10-24 18:03:13 -0300

    tpjs2

---
+++ TPJS/script.js
@@ -0,0 +1,21 @@
+const descuentoEstudiante = 0.8;
+const descuentoTrainee = 
+
+function calcularResumen(valorDescuento, valorEntrada, cantidadEntradas) {
+    const valorResumen;
+    return valorResumen
+}
+
+function calcularDescuento(tipoConsumidor) {
+    const valorDescuento;
+    return valorDescuento
+}
+
+const tipoConsumidor = document.getElementById('Lista')
+
+const descuentoCalculado = calcularDescuento(tipoConsumidor)
+
+const precio = 200
+const numeroEntradas = document.getElementById('Cantidad')
+
+const valorResumen = calcularResumen(descuentoCalculado, precio, numeroEntradas)
